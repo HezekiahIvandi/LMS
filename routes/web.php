5fcedd74598a071a2ed9b21542c948d59bfcf748
HezekiahIvandi <hezekiahivandi@gmail.com>
2024-06-01 13:16:44 +0700

    Inisialisasi

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\authController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//login page
+Route::get('/login', [authController::class,'login'])->name('login');
