89a68e84b78f0f4d1eb7a1f94a8a552eb2d3eee4
MarchellaAngelina <marchellangelina@gmail.com>
2024-06-05 15:47:46 +0700

    Menambahkan navigasi dan section 'kursus yang tersedia' pada halaman beranda

---
+++ resources/views/home.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="id">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @vite('resources/css/home.css')
+        @vite('resources/js/home.js')
+        <title>Belajar Daring Wikimedia Indonesia</title>
+    </head>
+
+    <body>
+        <!-- Navigation -->
+        <nav>
+            <img src="storage/Ikon WikiLatih.png" alt="Ikon WikiLatih">
+            <div class="navigation">
+                <ul>
+                    <li><a href="#">Beranda</a></li>
+                    <li><a href="#">Kursus</a></li>
+                    <li><a href="#">Artikel</a></li>
+                    <li><a href="#">Tentang Kami</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <!-- Home -->
+        <section id="home">
+            <h2>Belajar Daring Wikimedia Indonesia</h2>
+            <p>Kelas Daring Wikimedia Indonesia menyediakan materi-materi pelatihan untuk berkontribusi di proyek-proyek Wikimedia. Materi-materi yang tersedia di Kelas Daring Wikimedia Indonesia dapat diakses dan diikuti oleh publik secara daring (online). Kelas Daring Wikimedia Indonesia dikelola langsung oleh Wikimedia Indonesia.</p>
+            <div class="btn">
+                <a class="course-btn" href="#">Jelajahi Kursus</a>
+                <a class="about-btn" href="#">Tentang Kami</a>
+            </div>
+        </section>
+
+        <!-- Courses -->
+        <section id="course">
+            <h1>Kursus yang Tersedia</h1>
+            <p>WikiLatih Daring adalah program pelatihan penyuntingan di Wikipedia yang diadakan secara daring.</p>
+            <div class="course-box">
+                <div class="courses">
+                    <img src="storage/Kursus WikiLatih.png" alt="Ikon WikiLatih">
+                    <div class="details">
+                        <h4>WikiLatih Daring #16</h4>
+                        <p>Pelatih: Dian Agustin</p>
+                    </div>
+                </div>  
+                <div class="courses">
+                    <img src="storage/Kursus WikiLatih.png" alt="Ikon WikiLatih">
+                    <div class="details">
+                        <h4>WikiLatih Daring #16</h4>
+                        <p>Pelatih: Dian Agustin</p>
+                    </div>
+                </div>  
+                <div class="courses">
+                    <img src="storage/Kursus WikiLatih.png" alt="Ikon WikiLatih">
+                    <div class="details">
+                        <h4>WikiLatih Daring #16</h4>
+                        <p>Pelatih: Dian Agustin</p>
+                    </div>
+                </div>  
+                <div class="courses">
+                    <img src="storage/Kursus WikiLatih.png" alt="Ikon WikiLatih">
+                    <div class="details">
+                        <h4>WikiLatih Daring #16</h4>
+                        <p>Pelatih: Dian Agustin</p>
+                    </div>
+                </div>  
+            </div>
+        </section>
+    </body>
+</html>
